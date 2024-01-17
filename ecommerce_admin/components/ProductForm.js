aa56458e727f36a39f7ea7b9c485b6271ae3e34d
alif <aliftareq@gmail.com>
2024-01-17 10:32:47 +0600

    photo uploading api with aws created

--- ecommerce_admin/components/ProductForm.js
+++ ecommerce_admin/components/ProductForm.js
@@ -2,11 +2,14 @@ import axios from 'axios'
 import { useRouter } from 'next/router'
 import React, { useState } from 'react'
 
+
+
 const ProductForm = ({
     _id,
     title: extstingTitle,
     description: extstingDescription,
-    price: extstingPrice
+    price: extstingPrice,
+    images
 }) => {
     const [title, setTitle] = useState(extstingTitle || '')
     const [description, setDescription] = useState(extstingDescription || '')
@@ -15,9 +18,7 @@ const ProductForm = ({
 
     const router = useRouter()
 
-    //1:46
-
-
+    //functions
     const saveProduct = async (e) => {
         e.preventDefault()
         const data = {
@@ -36,6 +37,18 @@ const ProductForm = ({
         setGoToProducts(true)
     }
 
+    const uploadImages = async (ev) => {
+        const files = ev.target.files;
+        if (files.length > 0) {
+            const data = new FormData();
+            for (const file of files) {
+                data.append('file', file);
+            }
+            const res = await axios.post('/api/upload', data)
+            console.log(res.data)
+        }
+    }
+
     if (goToProducts) {
         router.push('/products')
     }
@@ -48,6 +61,21 @@ const ProductForm = ({
                 value={title}
                 onChange={(e) => setTitle(e.target.value)}
             />
+            <label>Photos</label>
+            <div className='mb-2'>
+                <label className='w-24 h-24 cursor-pointer text-center flex items-center justify-center text-sm gap-1 rounded-md bg-gray-200'>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5" />
+                    </svg>
+                    <div>
+                        Upload
+                    </div>
+                    <input type="file" onChange={uploadImages} className='hidden' />
+                </label>
+                {!images?.length && (
+                    <div>No Photos in this Product</div>
+                )}
+            </div>
             <label>Description</label>
             <textarea
                 placeholder='description'
